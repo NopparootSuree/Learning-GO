3b71b37f3aa11d5f65e9c5b9f28ebafc98f8f7dd
nopparootSuree <nopparoot.su@gmail.com>
2023-07-05 14:02:15 +0700

    edit NewServer and add endpoint

--- api/server.go
+++ api/server.go
@@ -3,22 +3,33 @@ package api
 import (
 	db "github.com/NopparootSuree/Learning-GO/db/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
+	_ "github.com/golang/mock/mockgen/model"
 )
 
 type Server struct {
-	store  *db.Store
+	store  db.Store
 	router *gin.Engine
 }
 
-func NewServer(store *db.Store) *Server {
+func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
+
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+
+	router.POST("/users", server.createUser)
 
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
 	router.DELETE("/accounts/:id", server.deleteAccount)
 	router.PUT("/accounts/:id", server.updateAccount)
+
+	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
 	return server
